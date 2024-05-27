dd0b1c8c1dca456a02671d05bd94b7f17be7838b
Kumar Saurav Jha <kumarsaurav.jha@gwu.edu>
2024-05-27 08:16:35 +0530

    addition of correct code

--- Assignment_3.py
+++ Assignment_3.py
@@ -7,27 +7,90 @@ api_key = '4bd0b251e15c9d8a96893a35bd1b1611'
 # %%
 import requests
 
-url = 'https://api.openweathermap.org/data/2.5/onecall'
+# Your API key
+# api_key = 'your_api_key'
 
-# Set the parameters
-params = {
-    'lat': 51.507351,
-    'lon':-0.127758,
-    'appid': api_key,
-    'units': 'metric'
-}
+# Set the API endpoint
+url = 'http://api.openweathermap.org/data/2.5/forecast'
 
-# Make the GET request
-response = requests.get(url, params=params)
+cities = ['Bombay', 'New Delhi', 'Kolkata', 'Pune', 'Doha']
 
-# Print the status code (should be 200)
-print(response.status_code)
+def get_weather_forecast(city):
+    # Set the parameters
+    params = {
+        'q': city,
+        'appid': api_key,
+        'units': 'metric'
+    }
+
+    # Make the GET request
+    response = requests.get(url, params=params)
+    # Check the status code
+    if response.status_code == 200:
+        # Convert the response to JSON
+        data = response.json()
+        
+        forecasted_list = data['list']
+        dates = []
+        temperatures = []
+        humidities = []
+        pressures = []
+
+        for list in forecasted_list:
+            dates.append(list['dt_txt'])
+            temperatures.append(list['main']['temp'])
+            humidities.append(list['main']['humidity'])
+            pressures.append(list['main']['pressure'])
+
+        df = pd.DataFrame({
+            'date': dates,
+            'temperature':temperatures,
+            'humidity':humidities,
+            'pressure':pressures,
+            'city':city
+        })
+
+        df.date = pd.to_datetime(df.date)
+
+        return df
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+
+
+all_data = pd.concat([get_weather_forecast(city) for city in cities])
+
+#%%
+plt.figure(figsize=(15, 10))
+
+bombay_data = all_data[all_data['city'] == 'Bombay']
+
+
 # %%
-import json
+plt.figure(figsize=(15, 15))
 
-# Convert the response to JSON
-data = response.json()
+plt.subplot(3, 1, 1)
+for city in cities:
+    city_data = all_data[all_data['city'] == city]
+    plt.plot(city_data['date'], city_data['temperature'], label=f'{city} Temperature (°C)')
+plt.title('5 Day Weather Forecast')
+plt.ylabel('Temperature (°C)')
+plt.legend()
 
-# Print the weather description
-print(data['weather'][0]['description'])
+plt.subplot(3, 1, 2)
+for city in cities:
+    city_data = all_data[all_data['city'] == city]
+    plt.plot(city_data['date'], city_data['pressure'], label=f'{city} Pressure (hPa)')
+plt.ylabel('Pressure (hPa)')
+plt.legend()
+
+plt.subplot(3, 1, 3)
+for city in cities:
+    city_data = all_data[all_data['city'] == city]
+    plt.plot(city_data['date'], city_data['humidity'], label=f'{city} Humidity (%)')
+plt.ylabel('Humidity (%)')
+plt.xlabel('Date')
+plt.legend()
+
+plt.tight_layout()
+plt.show()
 # %%
